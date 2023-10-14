0fe0e4e9b46cbc20f88daf9c63812f178ff17fd2
Rahel Cecilia <112471006+rahelcecilia@users.noreply.github.com>
2023-10-14 17:48:27 +0700

    Add files via upload

--- forms/quote.php
+++ forms/quote.php
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'Request for a quote';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -35,6 +35,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['phone'], 'Phone');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
